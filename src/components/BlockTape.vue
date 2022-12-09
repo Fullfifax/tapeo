deeda648cec441291c40deabac9e050772543489
Fullfifax <fullfifax1245@gmail.com>
2022-12-09 17:18:02 +0000

    add hooks lifecycle and timer

--- src/components/BlockTape.vue
+++ src/components/BlockTape.vue
@@ -1,24 +1,50 @@
 <template>
-    <div class="block">
-        Kitiooooooo ....
+    <div class="block" v-if="showBlock" @click="stopTimer">
+        Tapeoooo ....
     </div>
 </template>
 
 <script>
+
 export default {
-    props: ['delay']
+    props: ['delay'],
+    data(){
+        return {
+            showBlock: false,
+            timer: null,
+            reactionTime: 0
+        }
+    }, 
+    // Lifecycle hooks
+    mounted() {
+        setTimeout(() => {
+            this.showBlock = true
+            this.startTimer()
+        }, this.delay);
+    },
+    methods: {
+        startTimer() {
+            this.timer = setInterval(() => {
+                this.reactionTime += 10
+            }, 10)
+        },
+        stopTimer() {
+            clearInterval(this.timer)
+            console.log(this.reactionTime)
+        }
+    }
 }
 </script>
 
 <style>
 .block {
     background-color: #1565b6;
-    border-radius: 20px;
+    border-radius : 50%;
     color: white;
     font-size: 2em;
     margin: 40px auto;
     padding: 100px 0;
     text-align: center;
-    width: 350px;
+    width: 250px;
 }
 </style>
